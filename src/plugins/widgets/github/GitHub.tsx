ba84b82980eb9687b5909f4951177544757e0ee9
szamanr <szamanr@protonmail.com>
2024-07-25 11:18:09 +0200

    remove ref to old dependency

--- src/plugins/widgets/github/GitHub.tsx
+++ src/plugins/widgets/github/GitHub.tsx
@@ -2,8 +2,6 @@ import React, { FC, useEffect } from "react";
 import GitHubCalendar from "react-github-calendar";
 import { Props, defaultData } from "./types";
 
-import "github-calendar/dist/github-calendar.css";
-
 // TODO: Inherit size and colour
 
 const GitHubCalendarWidget: FC<Props> = ({ data = defaultData, loader }) => {
