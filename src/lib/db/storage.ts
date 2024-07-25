1018d34dc928ba6785c607bea29521eb0837fd02
szamanr <szamanr@protonmail.com>
2024-07-25 11:08:08 +0200

    install library and remove unused files

--- src/lib/db/storage.ts
+++ src/lib/db/storage.ts
@@ -144,7 +144,7 @@ const batch = (
   timeout = 0,
 ): DB.Listener => {
   const changes = new Map();
-  let timer: null | ReturnType<typeof setTimeout> = null;
+  let timer: NodeJS.Timeout | ReturnType<typeof setTimeout> = null;
 
   const run = () => {
     flush(changes);
